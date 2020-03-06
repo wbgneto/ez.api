95ce754c2eb014e57aa9be952454f9b2e0c79e24
Walter Galvão <walterbgneto@gmail.com>
2020-03-06 13:43:07 -0800

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ registerSchema(realtorValidationSchema);
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(Modules);
+  const app = await NestFactory.create(Modules, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT);
   Logger.log(`Server is running on http://localhost:${process.env.APP_PORT}`);
