4fecbd106e5fa2bcf1e91ec6e7311e7638cc558e
NazaninAhmadii <Nazaninpolite@gmail.com>
2020-03-25 15:03:00 -0700

    where clause type changed

--- src/modules/reports/reports.controller.ts
+++ src/modules/reports/reports.controller.ts
@@ -376,7 +376,7 @@ export class ReportsController {
                 .createQueryBuilder()
                 .select(["COUNT(DISTINCT(listing.id))  AS count" ,"YEAR(listing.sold_at) AS year", "MONTHNAME(listing.sold_at) AS month"])
                 .from(Listing, "listing")
-                .where("listing.status='2' AND listing.id IN (:...rid) AND listing.sold_at> :oneyear", {rid:ids, oneyear: oneYearAgo})
+                .where("listing.status='2' AND listing.type IN (:...rid) AND listing.sold_at> :oneyear", {rid:ids, oneyear: oneYearAgo})
                 .groupBy("year")
                 .addGroupBy("month")
                 .getRawMany();
