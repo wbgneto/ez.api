3c7d0a77121199bf930ba79faffc69f29e79a994
Walter Galvão <walterbgneto@gmail.com>
2020-04-01 18:09:36 -0700

    Follow up

--- src/modules/listings/data/updateListing.dto.ts
+++ src/modules/listings/data/updateListing.dto.ts
@@ -3,6 +3,7 @@ import {ListingType} from "./listingType.enum";
 import {Feature} from "../../features/feature.entity";
 import {Address} from "../../addresses/address.entity";
 import {Type} from "class-transformer";
+import {Realtor} from "../../realtors/realtor.entity";
 
 export class UpdateListingDto {
     @IsEnum(ListingType)
@@ -19,5 +20,5 @@ export class UpdateListingDto {
     address: Address;
 
     @IsOptional()
-    realtor: any;
+    realtor: Realtor;
 }
